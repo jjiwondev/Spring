09b33c470ccf6b698d6e7c5ea080fee30b3138a2
jjiwondev <nomppy@naver.com>
2023-06-09 13:41:49 +0900

    MVC2_ 6,7 cookie,session, filter interceptor

---
+++ 3_mvc2/5_login/src/main/java/hello/login/web/WebConfig.java
@@ -0,0 +1,59 @@
+package hello.login.web;
+
+import hello.login.web.argumentresolver.LoginMemberArgumentResolver;
+import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LoginCheckInterceptor;
+import hello.login.web.interceptor.LoginInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/logout",
+                        "/css/***", "/*.ico", "/error");
+
+    }
+
+//    @Bean
+    public FilterRegistrationBean logFilter(){
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);//순서를 정해주는 것
+        filterFilterRegistrationBean.addUrlPatterns("/*"); //모든 url을 가져옴
+
+        return filterFilterRegistrationBean;
+    }
+//    @Bean
+    public FilterRegistrationBean logCheckFilter(){
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterFilterRegistrationBean.setOrder(2);//순서를 정해주는 것
+        filterFilterRegistrationBean.addUrlPatterns("/*"); //모든 url을 가져옴
+
+        return filterFilterRegistrationBean;
+    }
+
+}
