77f2f24f896b623c81b6ae5fa740fc7c215eb263
jjiwondev <nomppy@naver.com>
2023-06-11 11:17:06 +0900

    JPA app1 start

---
+++ 3_mvc2/8_upload/file/19d8db79-c364-40b2-ade4-72c42b4f250b.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    
+    def say_hello(self):
+        print(f"안녕하세요. 제 이름은 {self.name}이고, 나이는 {self.age}입니다.")
+
